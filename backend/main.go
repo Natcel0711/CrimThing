665ae48b87524622950ad121da72f5d62bc713ee
Nat <natcelnieves@gmail.com>
2023-05-26 22:09:20 -0700

    Removed stuff

--- backend/main.go
+++ backend/main.go
@@ -1,27 +1,17 @@
 package main
 
 import (
-	"net/http"
 	"crim/handler"
+	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-type Property struct{
-	FirstName string `json:"firstname"`
-	LastName string `json:"lastname"`
-	Catastro string `json:"catastro"`
-	DireccionFisica string `json:"direccion_fisica"`
-	DireccionPostal string `json:"direccion_postal"`
-}
-
-func main(){
+func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Post("/", handler.GetProperty)
 
 	http.ListenAndServe(":3000", r)
 }
-
-
--- backend/models/property.go
+++ backend/models/property.go
@@ -1,47 +1,41 @@
 package models
 
-import(
+import (
 	"fmt"
 	"strings"
 )
 
-type Property struct{
-	FirstName string `json:"firstname"`
-	LastName string `json:"lastname"`
-	Catastro string `json:"catastro"`
+type Property struct {
+	FirstName       string `json:"firstname"`
+	LastName        string `json:"lastname"`
+	Catastro        string `json:"catastro"`
 	DireccionFisica string `json:"direccion_fisica"`
 	DireccionPostal string `json:"direccion_postal"`
 }
 
-
 func (p *Property) CreateUrl() string {
-	filters := []string{}
-
-	if p.FirstName != ""{
-		filters = append(filters, fmt.Sprintf("CONTACT LIKE '%s'", "%" + p.FirstName + "%"))
+	var filters []string
+	if p.FirstName != "" {
+		filters = append(filters, fmt.Sprintf("CONTACT LIKE '%s'", "%"+p.FirstName+"%"))
 	}
 
-
-	if p.LastName != ""{
-		filters = append(filters, fmt.Sprintf("CONTACT LIKE '%s'", "%" + p.LastName + "%"))
+	if p.LastName != "" {
+		filters = append(filters, fmt.Sprintf("CONTACT LIKE '%s'", "%"+p.LastName+"%"))
 	}
 
-
-	if p.Catastro != ""{
-		filters = append(filters, fmt.Sprintf("CATASTRO LIKE '%s'", "%" + p.Catastro + "%"))
+	if p.Catastro != "" {
+		filters = append(filters, fmt.Sprintf("CATASTRO LIKE '%s'", "%"+p.Catastro+"%"))
 	}
 
-
-	if p.DireccionFisica != ""{
-		filters = append(filters, fmt.Sprintf("DIRECCION_FISICA LIKE '%s'", "%" + p.DireccionFisica + "%"))
+	if p.DireccionFisica != "" {
+		filters = append(filters, fmt.Sprintf("DIRECCION_FISICA LIKE '%s'", "%"+p.DireccionFisica+"%"))
 	}
 
-
-	if p.DireccionPostal != ""{
-		filters = append(filters, fmt.Sprintf("DIRECCION_POSTAL LIKE '%s'", "%" + p.DireccionPostal + "%"))
+	if p.DireccionPostal != "" {
+		filters = append(filters, fmt.Sprintf("DIRECCION_POSTAL LIKE '%s'", "%"+p.DireccionPostal+"%"))
 	}
 
-	if len(filters) == 0{
+	if len(filters) == 0 {
 		return ""
 	}
 
